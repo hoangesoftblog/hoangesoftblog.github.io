bb8414d4a5b937fb1994010361e2713a37f6b690
hoangesoftblog <hoangviethoa123@yahoo.com.vn>
2023-12-07 10:51:16 +0700

    build: football-watch - Add "Custom room"

---
+++ apps/football-watch/src/app/custom-room/page.tsx
@@ -0,0 +1,15 @@
+// Todo: Add the match detail panel, using useEffect subscription
+export default function Page() {
+    console.log("Rendering Page: Custom Room");
+
+    console.log("Finishing Page: Custom Room");
+    
+    return (
+        <div className="">
+            <h1 className="text-3xl mb-4 font-bold">Custom Room</h1>
+            <div>
+                {/* <SocolivePlayer data={detail.data} /> */}
+            </div>
+        </div>
+    )
+}
